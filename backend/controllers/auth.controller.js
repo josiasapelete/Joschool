0df1b958eb283a9e2fcb7bb07f2f5a1e39828744
josiasapelete <josiasapelete@gmail.com>
2022-09-19 15:10:55 +0200

    login backend reparer

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -18,24 +18,30 @@ module.exports.signUp= async (req,res)=>{
 }
 
 
-const createToken = (id) => {
-    return jwt.sign({id}, process.env.TOKEN, {
-      expiresIn: maxAge
-    })
-  };
+
   module.exports.login = async (req, res) => {
-    const { email, password } = req.body
+    const {email,password}=req.body;
+    const user= await userModel.findOne({email});
+    const maxAge= 24*60*60*1000;
+    const SECRET_TOKEN=process.env.TOKEN;
   
-    try {
-      const user = await userModel.login(email, password);
-      const token = createToken(user._id);
-      res.cookie('jwt', token, { httpOnly: true, maxAge});
-      res.status(200).json({ user: user._id})
-    } catch (err){
-      const errors = signInErrors(err);
-      res.status(400).json({ errors });
-    }
-  }
+    
+      if(user){
+        const verifyPass= await bcrypt.compare(password,user.password);
+
+       if(!verifyPass){
+        res.status(400).json("Wrong password");
+       } else{
+        const token= jwt.sign({
+            email: user.email, id: user._id
+        },SECRET_TOKEN,{expiresIn: maxAge});
+        res.cookie("jwt",token,{httpOnly:true, maxAge})
+        res.status(200).json({user,token})
+       }
+        } else{
+            res.status(404).json("User does'nt exist");
+        }
+    } 
   
 
 module.exports.logout= (req,res)=>{
